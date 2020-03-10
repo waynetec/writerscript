802b4da574f4f97d6b9585bbd351b227c56af1d0
waynetec <46908942+waynetec@users.noreply.github.com>
2020-03-10 15:06:52 -0400

    Add files via upload
    Updated with user variables

--- book.sh
+++ book.sh
@@ -2,16 +2,31 @@
 
 ##A shell script to create chapter folders and empty scenes##
 
-mkdir MyGreatNovel 		##<--Creates the master directory for book##
-cd MyGreatNovel			##<--Enters the master directory to execute loops##
+## SET NAME OF YOUR NOVEL HERE. REPLACE MyNovelName with name of your novel. No spaces allowed##
+NovelName=BookName       
 
-for ((i=1; i<=20; i++)) ##<--Change 20 to number of chapter folders desired##
+## SET PREFIX FOR EACH CHAPTER ##
+ChapterName=Chapter 
+
+## SET PREFIX FOR EACH SCENE##
+SceneName=Scene
+
+##<--Change 20 to number of chapter directories desired##	
+d=20
+
+##<--Change 10 to number of scenes in each chapter directory##					 
+s=10					 		
+
+mkdir -p $NovelName		 ##<--Creates the master directory for book##
+cd $NovelName			 ##<--Enters the master directory to execute loops##
+
+for ((i=1; i<=$d; i++)) 	
 do
-    mkdir Chapter$i 	##<--Rename chapter directories to desired name##
-    cd Chapter$i 		##Enters currently created directory##
-    for((j=1;j<=10;j++))##<--Change 10 to number of scenes in each chapter folder##
+    mkdir $ChapterName$i ##<--Rename chapter directories to desired name##
+    cd $ChapterName$i 	 ##Enters currently created directory##
+    for((j=1;j<=$s;j++))	
     do
-        touch Scene$j;	##Creates scenes in directory##
+        touch $SceneName$j;	 ##Creates scenes in directory##
     done
-    cd .. 				##Back out of directory to create next directory or finish##
+    cd .. 				 ##Back out of directory to create next directory or finish##
 done
